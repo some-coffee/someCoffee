35474dcf3d8323e857970c8755b987c632d38c01
aaldosari1 <dosr@live.com>
2021-12-12 11:51:34 +0300

    lets begin

---
+++ some_coffee/src/main/java/com/example/some_coffee/SomeCoffeeApplication.java
@@ -0,0 +1,13 @@
+package com.example.some_coffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomeCoffeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomeCoffeeApplication.class, args);
+	}
+
+}
---
+++ some_coffee/src/test/java/com/example/some_coffee/SomeCoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.some_coffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomeCoffeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
